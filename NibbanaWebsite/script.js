63abf8b5495fea928fa64988a964dd09d44b5aa5
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-04-30 12:17:16 +0530

    30 April 2025 - Wednesday

---
+++ NibbanaWebsite/script.js
@@ -0,0 +1,6 @@
+function showDropDown() {
+    document.querySelector(".profileContainer").classList.add('active');
+}
+function hideDropDown() {
+    document.querySelector(".profileContainer").classList.remove('active');
+}
