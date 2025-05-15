ebaccc748376ce1857232173f616ed43b7070782
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-05-15 17:45:50 +0530

    15 May 2025 - Thursday

--- NibbanaWebsite/script.js
+++
@@ -1,6 +0,0 @@
-function showDropDown() {
-    document.querySelector(".profileContainer").classList.add('active');
-}
-function hideDropDown() {
-    document.querySelector(".profileContainer").classList.remove('active');
-}
