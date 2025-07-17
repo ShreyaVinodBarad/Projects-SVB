1a75da60592371f4b7de8d1ced8bbf4ea62c070d
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-07-17 09:31:23 +0530

    17 July 2025 - Thursday

---
+++ Project-10_WeatherApp/script.js
@@ -0,0 +1,24 @@
+/*
+1) We will use the API to get weather information when ever we type any city name.
+
+2) What is API?
+- An API is a bridge that allows different apps or software to communicate and work together.
+- In tech terms:
+a) An API allows one program to talk to another program.
+b) It defines a set of rules for how programs can request and send information to each other.
+c) It hides the complex details and lets you get what you need easily.
+
+3) Procedure:
+a) Go to Open Weather Map
+b) Create an Account
+c) Verify your email
+d) Go to API section
+e) Go to current weather Data
+f) In this section scroll down and copy API Call's first option and paste it in the Chrome.
+g) The link is something like this here you have to write the name of city in the city name and enter API Key in it's part. Here your API Key is:
+d4c4d289ec44bf44d8ac7c6021704aeb
+https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+h) The API key takes some couple of hours to Activate then you will get the Weather information in JSON Format.
+
+3) 
+*/ 
