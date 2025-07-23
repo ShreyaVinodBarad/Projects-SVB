b56936f7ce8acc5ec8a0e35a772c26091011b7b4
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-07-23 14:05:55 +0530

    23 July 2025 - Wednesday

--- Project-10_WeatherApp/script.js
+++ Project-10_WeatherApp/script.js
@@ -18,4 +18,65 @@ f) In this section scroll down and copy API Call's first option and paste it in 
 g) There will a link something like this here you have to write the name of city in the city name and enter API Key in it's part.
 https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 h) The API key takes some couple of hours to Activate then you will get the Weather information in JSON Format.
-*/ 
+*/
+
+const apiKey = "d4c4d289ec44bf44d8ac7c6021704aeb";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+/*
+👆
+We need to use metric to get temp in Celcius format.
+*/
+const searchBox = document.getElementById("searchBox");
+const searchBtn = document.getElementById("searchBtn");
+const weatherImg = document.getElementById("weatherImg");
+async function checkWeather(city) {
+    /*
+    👆
+    This asynchronous function fetches weather data for the provided city
+    */
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    /*
+    👆
+    Makes a GET request to the API
+    */
+    if (response.status == 404) {
+        document.getElementById("error").style.display = "block";
+        document.getElementById("weather").style.display = "none";
+    }
+    else {
+        let data = await response.json();
+        /*
+        👆
+        Convert response to JSON.
+        */
+        console.log(data);
+        document.getElementById("city").innerHTML = data.name;
+        document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.getElementById("humidity").innerHTML = data.main.humidity + "%";
+        document.getElementById("wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherImg.src = "./Assets/CloudsImg.png"
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherImg.src = "./Assets/ClearImg.png"
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherImg.src = "./Assets/RainImg.png"
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherImg.src = "./Assets/DrizzleImg.png"
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherImg.src = "./Assets/MistImg.png"
+        }
+        else if (data.weather[0].main == "Snow") {
+            weatherImg.src = "./Assets/SnowImg.png"
+        }
+        document.getElementById("weather").style.display = "block";
+        document.getElementById("error").style.display = "none";
+    }
+}
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
